1f3e4223d456cbd8a82329153608208dbf44e958
Mariana Lima <marianadsantoslima@gmail.com>
2025-09-29 08:42:30 +0000

    Projeto em C simulando o movimento do cavalo no xadrez. Nele aplicamos entrada de dados com scanf, laço while para controlar a execução e laço for para repetir movimentos específicos. O foco foi praticar estruturas de repetição aninhadas e a lógica de simulação de movimentos em um tabuleiro.

---
+++ aventureiro.T4.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(){
+
+    /*MOVIMENTANDO PEÇAS DE XADREZ*/
+
+    int comando = 0;
+
+    printf("Digite 1. para mover o CAVALO:\n");
+    scanf("%d",&comando);
+
+
+    int movimento = 1;
+    
+    while (movimento--){
+
+       for(int i = 0; i < 2 ; i++){
+          printf("cima\n");
+    }
+
+    printf("Direita");
+
+    }
+
+    return 0;
+}
+    
+  
